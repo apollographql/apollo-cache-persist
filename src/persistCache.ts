89423b26de42272bbcbeaf9003827c669b7897f2
Peggy Rayzis <peggyrayzis@gmail.com>
2017-12-10 15:17:22 -0500

    Refactored mocks & tests, added error messages to CachePeristor

--- src/persistCache.ts
+++ src/persistCache.ts
@@ -2,20 +2,6 @@ import CachePersistor from './CachePersistor';
 import { ApolloPersistOptions } from './types';
 
 export default <T>(options: ApolloPersistOptions<T>) => {
-  if (!options.cache) {
-    throw new Error(`
-      In order to persist your Apollo Cache, you need to pass in a cache.
-      Please see https://www.apollographql.com/docs/react/basics/caching.html for our default InMemoryCache.
-    `);
-  }
-
-  if (!options.storage) {
-    throw new Error(`
-      In order to persist your Apollo Cache, you need to pass in an underlying storage provider.
-      Please see https://github.com/apollographql/apollo-cache-persist#storage-providers
-    `);
-  }
-
   const persistor = new CachePersistor(options);
   return persistor.restore();
 };
