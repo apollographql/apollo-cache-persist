bf54ee298ff399cf1902c801a97b31b95fea0058
James Reggio <james.reggio@gmail.com>
2017-12-19 13:17:57 -0500

    Fix #17: Modify storage types to accept synchronous results

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,12 +8,12 @@ export type TriggerUninstallFunction = () => void;
 
 export type TriggerFunction = (persist: () => void) => TriggerUninstallFunction;
 
-export type PersistedData<T> = T | string;
+export type PersistedData<T> = T | string | null;
 
 export interface PersistentStorage<T> {
-  getItem: (key: string) => Promise<T>;
-  setItem: (key: string, data: T) => Promise<void>;
-  removeItem: (key: string) => Promise<void>;
+  getItem: (key: string) => Promise<T> | T;
+  setItem: (key: string, data: T) => Promise<void> | void;
+  removeItem: (key: string) => Promise<void> | void;
 }
 
 export interface ApolloPersistOptions<TSerialized> {
