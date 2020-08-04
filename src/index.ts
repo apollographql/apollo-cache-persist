313fd066413c613cdc5602cfb0d69bde34b34de4
Simen Bekkhus <sbekkhus91@gmail.com>
2020-08-04 10:56:32 +0200

    fix: export `PersistentStorage` type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as CachePersistor } from './CachePersistor';
 export { default as persistCache } from './persistCache';
 export * from './persistCacheSync';
+export { PersistentStorage } from './types';
