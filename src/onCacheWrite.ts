5b086f33ee2bdbc23d7bc113a35f1a7d30308559
Fándly Gergő <gergo@systemtest.tk>
2020-09-18 11:53:36 +0300

    feat: Add trigger for cache.evict and cache.modify, return results of functions

--- src/onCacheWrite.ts
+++ src/onCacheWrite.ts
@@ -8,13 +8,28 @@ export default <T>({ cache }: TriggerFunctionConfig<T>) => (
   persist: () => void,
 ) => {
   const write = cache.write;
+  const evict = cache.evict;
+  const modify = cache.modify;
+
   cache.write = (...args: any[]) => {
-    const ref = write.apply(cache, args);
+    const result = write.apply(cache, args);
     persist();
-    return ref;
+    return result;
+  };
+  cache.evict = (...args: any[]) => {
+    const result = evict.apply(cache, args);
+    persist();
+    return result;
+  };
+  cache.modify = (...args: any[]) => {
+    const result = modify.apply(cache, args);
+    persist();
+    return result;
   };
 
   return () => {
     cache.write = write;
+    cache.evict = evict;
+    cache.modify = modify;
   };
 };
