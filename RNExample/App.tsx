d4645f541cfb10eaf2d8e9c8791371dd78ca76ad
ptrocki <pan.stroncky@gmail.com>
2020-09-29 17:15:49 +0200

    create a simple example

--- RNExample/App.tsx
+++ RNExample/App.tsx
@@ -8,30 +8,19 @@
  * @format
  */
 
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, Text} from 'react-native';
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {persistCache} from 'apollo3-cache-persist';
 import AsyncStorage from '@react-native-community/async-storage';
+import {gql} from '@apollo/client';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [error, setError] = useState('');
+
   useEffect(() => {
     async function init() {
       const cache = new InMemoryCache();
@@ -43,52 +32,48 @@ const App = () => {
         uri: 'https://48p1r2roz4.sse.codesandbox.io',
         cache: new InMemoryCache(),
       });
+      try {
+        const {data} = await client.query({
+          query: gql`
+            query GetRates {
+              rates(currency: "USD") {
+                currency
+              }
+            }
+          `,
+        });
+
+        //@ts-ignore type this
+        setCurrencies(data.rates);
+      } catch (e) {
+        setError(e.message);
+      }
     }
     init();
   }, []);
 
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        {error ? (
+          <Text style={{padding: 16, fontWeight: 'bold'}}>{error}</Text>
+        ) : (
+          <>
+            <Text style={{padding: 16, fontWeight: 'bold'}}>
+              List of currencies
+            </Text>
+            <ScrollView
+              contentInsetAdjustmentBehavior="automatic"
+              style={styles.scrollView}>
+              {currencies.map((item, index) => (
+                //@ts-ignore type it
+                <Text key={item.currency + String(index)} style={{padding: 16}}>
+                  {item.currency}
+                </Text>
+              ))}
+            </ScrollView>
+          </>
+        )}
       </SafeAreaView>
     </>
   );
@@ -97,39 +82,6 @@ const App = () => {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
--- RNExample/babel.config.js
+++ RNExample/babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          'apollo3-cache-persist': '../src',
-        },
-        cwd: 'babelrc',
-      },
-    ],
-  ],
 };
