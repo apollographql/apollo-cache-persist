c4b1f362235e6cf855aa9be85f3c0d357bef310b
ptrocki <pan.stroncky@gmail.com>
2020-09-29 16:08:51 +0200

    add necessary deps

--- RNExample/babel.config.js
+++ RNExample/babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'apollo3-cache-persist': '../src',
+        },
+        cwd: 'babelrc',
+      },
+    ],
+  ],
 };
