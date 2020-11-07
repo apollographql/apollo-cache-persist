4891c41ddc9411451aad0f55b3baa1d3c4dd145a
Martin Janeček <admin@ikw.cz>
2020-11-07 15:02:11 +0100

    fix bundling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as CachePersistor } from './CachePersistor';
 export { default as persistCache } from './persistCache';
 export * from './persistCacheSync';
-export * from './storageWrappers';
+export * from './storageWrappers/index';
 export { PersistentStorage } from './types';
