56b1c5513505900b8bf87a289994d6fc3505ccd5
Martin Janeček <admin@ikw.cz>
2021-08-02 19:28:18 +0200

    fix(types): provide simplistic storage interfaces
    fix #426, fix #431 by adding support for new synchronous removeItem

--- src/storageWrappers/IonicStorageWrapper.ts
+++ src/storageWrappers/IonicStorageWrapper.ts
@@ -1,22 +1,28 @@
 import { PersistentStorage } from '../types';
 
-export class IonicStorageWrapper implements PersistentStorage<string> {
-  // Actual type definition: https://github.com/ionic-team/ionic-storage/blob/main/src/storage.ts#L102
+export class IonicStorageWrapper implements PersistentStorage<string | null> {
   private storage;
 
-  constructor(storage: any) {
+  constructor(storage: IonicStorageInterface) {
     this.storage = storage;
   }
 
-  getItem(key: string): string | Promise<string | null> | null {
+  getItem(key: string): Promise<string | null> {
     return this.storage.get(key);
   }
 
-  removeItem(key: string): void | Promise<void> {
+  removeItem(key: string): Promise<void> {
     return this.storage.remove(key);
   }
 
-  setItem(key: string, value: string): void | Promise<void> {
+  setItem(key: string, value: string | null): Promise<void> {
     return this.storage.set(key, value);
   }
+}
+
+interface IonicStorageInterface {
+  // Actual type definition: https://github.com/ionic-team/ionic-storage/blob/main/lib/src/index.ts
+  get(key: string): Promise<string | null>;
+  set(key: string, value: string | null): Promise<void>;
+  remove(key: string): Promise<void>;
 }
--- src/storageWrappers/LocalStorageWrapper.ts
+++ src/storageWrappers/LocalStorageWrapper.ts
@@ -1,22 +1,33 @@
 import { PersistentStorage } from '../types';
 
-export class LocalStorageWrapper implements PersistentStorage<string> {
-  // Actual type definition: https://github.com/microsoft/TypeScript/blob/master/lib/lib.dom.d.ts#L15286
+export class LocalStorageWrapper implements PersistentStorage<string | null> {
   private storage;
 
-  constructor(storage: any) {
+  constructor(storage: LocalStorageInterface) {
     this.storage = storage;
   }
 
-  getItem(key: string): string | Promise<string | null> | null {
+  getItem(key: string): string | null {
     return this.storage.getItem(key);
   }
 
-  removeItem(key: string): void | Promise<void> {
+  removeItem(key: string): void {
     return this.storage.removeItem(key);
   }
 
-  setItem(key: string, value: string): void | Promise<void> {
-    return this.storage.setItem(key, value);
+  setItem(key: string, value: string | null): void {
+    if (value !== null) {
+      // setting null to localstorage stores "null" as string
+      return this.storage.setItem(key, value);
+    } else {
+      return this.removeItem(key);
+    }
   }
+}
+
+interface LocalStorageInterface {
+  // Actual type definition: https://github.com/microsoft/TypeScript/blob/main/lib/lib.dom.d.ts#L14276
+  getItem(key: string): string | null;
+  setItem(key: string, value: string): void;
+  removeItem(key: string): void;
 }
