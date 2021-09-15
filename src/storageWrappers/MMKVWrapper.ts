1b16bdb00e896f62ee379777f7224a156e16004f
Thibault Malbranche <malbranche.thibault@gmail.com>
2021-09-15 17:45:47 +0200

    feat(MMKVWrapper): Added new storage wrapper
    Added storage wrapper for react-native-mmkv.
    Storage wrapper already existed for another mmvk lib, but they don't have the same interface

---
+++ src/storageWrappers/MMKVWrapper.ts
@@ -0,0 +1,46 @@
+import { PersistentStorage } from '../types';
+
+/**
+ * Wrapper for react-native-mmkv.
+ * See [https://github.com/mrousavy/react-native-mmkv](https://github.com/mrousavy/react-native-mmkv) for installation instructions.
+ *
+ * @example
+ * const storage = new MMKV();
+ * const persistor = new CachePersistor({
+ *   cache,
+ *   storage: new MMKVWrapper(storage),
+ * });
+ *
+ */
+export class MMKVWrapper implements PersistentStorage<string | null> {
+  private storage;
+
+  constructor(storage: MMKVInterface) {
+    this.storage = storage;
+  }
+
+  getItem(key: string): string | null {
+    return this.storage.getString(key) || null;
+  }
+
+  removeItem(key: string): void {
+    return this.storage.delete(key);
+  }
+
+  setItem(key: string, value: string | null): void {
+    if (value !== null) {
+      return this.storage.set(key, value);
+    }
+    return this.removeItem(key);
+  }
+}
+
+interface MMKVInterface {
+    set: (key: string, value: boolean | string | number) => void;
+    getBoolean: (key: string) => boolean;
+    getString: (key: string) => string | undefined;
+    getNumber: (key: string) => number;
+    delete: (key: string) => void;
+    getAllKeys: () => string[];
+    clearAll: () => void;
+}
